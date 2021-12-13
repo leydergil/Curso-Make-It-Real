5572e0929fff663379c5853c8c7d9816a520a0fe
leyder <leyder.gil@gmail.com>
2021-12-12 23:22:55 -0500

    Agregando el ejercicio 1

---
+++ ejercicios/dia1/ejercicio1.js
@@ -0,0 +1,16 @@
+//Introducción a Java Script
+
+//imprime un mensaje por consola
+console.log('Hola Makers, welcome to Maka It real');
+
+//Asigna un mensaje a una varible y la imprime por consola.
+let mensaje = 'Hola Makers';
+console.log(mensaje);
+
+//Pide al usuario ingresar un mensaje y lo imprime por consola.
+let msg = prompt('Hola Maker, Ingresa un Mensaje por favor :');
+console.log(msg);
+
+//Otra forma de mostrar el resultado
+msg = mensaje;
+console.log(`La frase ingresda es: ${msg}`);
