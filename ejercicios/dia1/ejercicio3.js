8278fbc3545aa83704cbf54787537f259cc480a1
leyder <leyder.gil@gmail.com>
2021-12-12 23:57:08 -0500

    Agregando el ejercicio 3

---
+++ ejercicios/dia1/ejercicio3.js
@@ -0,0 +1,10 @@
+// /* Escribe un programa que le pida al usuario 
+// ingresar un número, luego le pide un segundo número, 
+// e imprima en la consola la suma de los dos números 
+// que ingresó el usuario. */
+
+let primerNumero = prompt("Ingrese el primer número: ");
+let segundoNumero = prompt("Ingrese el segundo número: ");
+
+let suma = parseInt(primerNumero + segundoNumero);
+console.log(`El resultado de la suma es : ${suma}`);
