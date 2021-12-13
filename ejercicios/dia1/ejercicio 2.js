ce4491ef300b3f7a587b998a3f041af9519ebdad
leyder <leyder.gil@gmail.com>
2021-12-12 23:39:20 -0500

    Segundo ejercicio

---
+++ ejercicios/dia1/ejercicio 2.js
@@ -0,0 +1,9 @@
+/*Escribe un programa que le pregunte al usuario su nombre e imprima "Hola " seguido del nombre y un signo de exclamación.
+Por ejemplo, si el usuario ingresa "Pedro" el programa debe imprimir en la consola "Hola Pedro!". */
+
+let nombreUsuario = prompt("Ingrese su nombre: ");
+console.log(`Hola ${nombreUsuario}!`);
+
+//otra forma
+nombreUsuario = "Leyder";
+console.log("Hola " + nombreUsuario + "!");
