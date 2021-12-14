4393544d66232d72c7dc9e1fa20c1b45d7a783bf
José Leyder Gil López <31496801+leydergil@users.noreply.github.com>
2021-12-13 21:50:44 -0500

    Update ejercicio 7

--- ejercicios/dia2/ejercicio7.js
+++ ejercicios/dia2/ejercicio7.js
@@ -1,5 +1,8 @@
 /*
-Escribe un programa que piense un número de forma aleatoria del 1 al 10 y le pida al usuario que lo trate de adivinar. Si el número es correcto debe imprimir en la consola "Felicitaciones, ese era!", de lo contrario debe imprimir "Lo siento, intenta nuevamente!"
+Escribe un programa que piense un número de forma aleatoria del 1 al 10 
+y le pida al usuario que lo trate de adivinar. Si el número es correcto 
+debe imprimir en la consola "Felicitaciones, ese era!", 
+de lo contrario debe imprimir "Lo siento, intenta nuevamente!"
 */
 
 const numeroAleatorio = Math.floor((Math.random() * 10)+1);
