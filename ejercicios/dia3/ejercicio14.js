3ef64dd9ceb6b1397817f7b128729b24d736f194
leyder <leyder.gil@gmail.com>
2022-01-04 19:05:19 -0500

    Ejercicio 14

---
+++ ejercicios/dia3/ejercicio14.js
@@ -0,0 +1,15 @@
+/* Ejercicio 14
+Escribe un programa para la consola que le pida al usuario un número 
+e imprima los números desde 1 hasta el número.
+
+Por ejemplo, si el usuario ingresa el número 5, el programa de be imprimir: 
+1
+2
+3
+4
+5
+*/
+var num = 8;
+for (let index = 0; index < num; index++) {
+    console.log(index + 1);   
+}
